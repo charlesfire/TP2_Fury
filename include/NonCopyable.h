7dfb0d401e045ca50c768618dd4b4af613c34dff
Charlesfire <charlesfire2@hotmail.com>
2015-10-29 14:50:55 -0400

    Ajout du singleton template

---
+++ include/NonCopyable.h
@@ -0,0 +1,18 @@
+#ifndef FURY_NONCOPYABLE_H
+#define FURY_NONCOPYABLE_H
+
+namespace Fury
+{
+    class NonCopyable
+    {
+        public:
+            ~NonCopyable();
+        protected:
+            NonCopyable() = default;
+        private:
+            NonCopyable(const NonCopyable& other) = delete;
+            NonCopyable& operator=(const NonCopyable& other) = delete;
+    };
+}
+
+#endif // FURY_NONCOPYABLE_H
--- include/Singleton.h
+++ include/Singleton.h
@@ -1,12 +1,22 @@
 #ifndef FURY_SINGLETON_H
 #define FURY_SINGLETON_H
 
+#include "NonCopyable.h"
 
-namespace Fury {
-
-class Singleton
-{};
-
+namespace Fury
+{
+    template<class T>
+    class Singleton : NonCopyable
+    {
+        public:
+            T& GetInstance()const
+            {
+                static T instance;
+                return instance;
+            }
+        protected:
+            Singleton() = default;
+    };
 } /* End of namespace Fury */
 
 #endif // FURY_SINGLETON_H
