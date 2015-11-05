cade289677f92ddd57462b2cfe237bd5e360ba99
Sakuretsu <korvax1@gmail.com>
2015-11-05 13:02:18 -0500

    Direction.h

---
+++ include/Direction.h
@@ -0,0 +1,19 @@
+#ifndef FURY_ENUMS_H
+#define FURY_ENUMS_H
+
+namespace Fury
+{
+    enum Direction
+    {
+        West,
+        NorthWest,
+        North,
+        NorthEast,
+        East,
+        SouthEast,
+        South,
+        SouthWest
+    };
+}
+
+#endif // FURY_ENUMS_H
