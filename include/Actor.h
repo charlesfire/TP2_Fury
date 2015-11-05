a85ccf8d1adfca7a83d602473bcb75d86b3bfc58
Sakuretsu <korvax1@gmail.com>
2015-11-05 12:43:33 -0500

    Actor.h

--- include/Actor.h
+++ include/Actor.h
@@ -1,11 +1,14 @@
 #ifndef FURY_ACTOR_H
 #define FURY_ACTOR_H
 
+#include <SFML/Graphics/Drawable.hpp>
+#include "PhysicBody.h"
 
-namespace Fury {
-
-class Actor
-{};
+namespace Fury 
+{
+	class Actor : public PhysicBody, public sf::Drawable
+	{
+	};
 
 } /* End of namespace Fury */
 
