190d2de3b0ae6425c70a46918e043bdc65f872d8
Charlesfire <charlesfire2@hotmail.com>
2015-11-05 14:24:37 -0500

    Ajout de tests de type

--- include/World.hpp
+++ include/World.hpp
@@ -1,7 +1,9 @@
 #ifndef FURY_WORLD_HPP
 #define FURY_WORLD_HPP
 
+#include <type_traits>
 #include <vector>
+#include "mld.hpp"
 
 namespace Fury
 {
@@ -15,6 +17,7 @@ namespace Fury
             template<class T, class ...Args>
             T* AddActor(Args... args)
             {
+                static_assert(std::is_base_of<Actor, T>::value, "Only class derived from Actor could be added to a world.");
                 T* temp = new T(args...);
                 actors.push_back(temp);
                 return temp;
