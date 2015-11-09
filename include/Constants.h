a3ad428e29c52965e13b1127bf05aa286cc735cf
Charlesfire <charlesfire2@hotmail.com>
2015-11-09 09:11:49 -0500

    Progression de la génération d'une salle

--- include/Constants.h
+++ include/Constants.h
@@ -6,6 +6,8 @@ namespace Fury
     const unsigned int WINDOW_WIDTH = 1280;
     const unsigned int WINDOW_HEIGHT = 720;
 	const unsigned int PLAYER_SHOT_DELAY = 250;
+	const float WALL_THICKNESS = 15.f;
+	const float WALL_LENGTH = 220.f;
 }
 
 #endif // FURY_CONSTANTS_H_INCLUDED
