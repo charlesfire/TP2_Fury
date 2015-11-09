cff3718967bb50afe1cb3b36ee93f0f5ca4897ab
Sakuretsu <korvax1@gmail.com>
2015-11-09 09:00:41 -0500

    Repair Projectile

--- include/Projectile.h
+++ include/Projectile.h
@@ -1,11 +1,27 @@
 #ifndef FURY_PROJECTILE_H
 #define FURY_PROJECTILE_H
 
+#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include "CircleShape.h"
+#include "PhysicActor.h"
 
-namespace Fury {
+namespace Fury
+{
+	class World;
 
-class Projectile
-{};
+	class Projectile : public PhysicActor
+	{
+	public:
+		Projectile(World * world, const sf::Vector2f& speed);
+		~Projectile() = default;
+		void Update();
+		void draw(sf::RenderTarget& target, sf::RenderStates state)const;
+	private:
+		sf::Sprite sprite;
+		CircleShape hitCircle;
+	};
 
 } /* End of namespace Fury */
 
--- src/Projectile.cpp
+++ src/Projectile.cpp
@@ -1,7 +1,21 @@
 #include "Projectile.h"
-#include "mld.hpp"
+#include "Shape.h"
+#include "TextureManager.hpp"
+#include "World.hpp"
 
 namespace Fury
 {
-
+	Projectile::Projectile(World * world, const sf::Vector2f& speed) : hitCircle(5), sprite()
+	{
+		SetVelocity(speed);
+		sprite.setTexture(TextureManager::GetInstance().GetTexture("Sprites/Balle.png"));
+	}
+	void Projectile::Update()
+	{
+		sprite.setPosition(GetPosition());
+	}
+	void Projectile::draw(sf::RenderTarget& target, sf::RenderStates state)const
+	{
+		target.draw(sprite);
+	}
 }
